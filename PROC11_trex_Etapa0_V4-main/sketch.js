dd4160ac71b23dba0f62f52a61c2a9ac03f369fd
ANPP3382 <118580473+ANPP3382@users.noreply.github.com>
2022-11-25 20:18:36 -0600

    Add files via upload

---
+++ PROC11_trex_Etapa0_V4-main/sketch.js
@@ -0,0 +1,41 @@
+
+var trex ,trex_running;
+var edges;
+var ground, groundImg, invisible;
+function preload(){
+ trex_running = loadAnimation("trex1.png","trex3.png", "trex4.png");
+groundImg=loadImage("ground2.png");
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crear sprite de Trex
+  trex = createSprite(50, 70, 20, 50)
+  edges = createEdgeSprites();
+  trex.addAnimation("running", trex_running);
+  trex.scale=0.5;
+  
+//Piso
+ground= createSprite(180, 180, 600, 20);
+ground.addImage(groundImg)
+invisible=createSprite(200,190,600,10);
+invisible.visible=false;
+}
+
+function draw(){
+  background("orange")
+
+  ground.velocityX=-2; 
+  if(ground.x < 0){ 
+    ground.x=ground.width/2; 
+  }
+ 
+  if(keyDown("space") && trex.y>=100){
+    trex.velocityY = -10
+}
+trex.velocityY=trex.velocityY +0.8;
+ trex.collide(invisible); 
+  
+drawSprites();
+}
